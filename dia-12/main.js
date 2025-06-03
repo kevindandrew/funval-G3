06de0bea47f0f5e33c39b56ca16d6a171607fadc
kevindandrew <kevindandrew77@gmail.com>
2025-06-03 16:21:55 -0400

    ejercicio homeros

--- dia-12/main.js
+++ dia-12/main.js
@@ -153,3 +153,142 @@ switch (dia) {
 // 7. Muestra el número de días que tiene un mes dependiendo de la variable del ejercicio anterior
 
 // 8. Usa un switch para imprimir un mensaje de saludo diferente dependiendo del idioma
+
+/* === == */
+/* ejercicio 1 */
+let variable1 = "Johan";
+
+if (variable1 === "Kevin") {
+  console.log("mi nombre es Kevin");
+}
+/* ejercicio 2 */
+let password = "123456";
+let username = "user_kevin";
+
+password == 123456 && username === "user_kevin"
+  ? console.log("datos correctos thomas")
+  : console.log("datos incorrectos thomas");
+
+/* if (password == 123456 && username === "user_kevin") {
+  console.log("datos correctos");
+}
+ */
+
+let numeroAleatorio = -1;
+
+if (numeroAleatorio > 0) {
+  console.log("este numero es positivo");
+} else if (numeroAleatorio === 0) {
+  console.log("este numero es cero");
+} else {
+  console.log("este numero es negativo");
+}
+
+let edadVoto = 12;
+if (edadVoto >= 18) {
+  console.log("puedes votar amigo ");
+} else {
+  let edadFaltante = 0;
+  edadFaltante = 18 - edadVoto;
+  console.log("no puedes votar te faltan :" + edadFaltante + " años");
+}
+
+let edadPersona = 22;
+let estadoPersona;
+edadPersona >= 18 ? (estadoPersona = "Adulto") : (estadoPersona = "menor");
+
+console.log(estadoPersona);
+
+let mes = "septiembre";
+let diaMes = 20;
+switch (mes) {
+  case "enero":
+    console.log("verano", "31 dias");
+    break;
+  case "febrero":
+    console.log("verano", "28 dias");
+    break;
+  case "marzo":
+    console.log("otoño", "31 dias");
+    break;
+  case "abril":
+    console.log("otoño", "30 dias");
+    break;
+  case "mayo":
+    console.log("otoño", "31 dias");
+    break;
+  case "junio":
+    console.log("invierno", "30 dias");
+    break;
+  case "julio":
+    console.log("invierno", "31 dias");
+    break;
+  case "agosto":
+    console.log("invierno", "31 dias");
+    break;
+  case "septiembre":
+    console.log("primavera", "30 dias");
+    break;
+  case "octubre":
+    console.log("primavera", "31 dias");
+    break;
+  case "noviembre":
+    console.log("primavera", "30 dias");
+    break;
+  case "diciembre":
+    console.log("verano", "31 dias");
+    break;
+  default:
+    break;
+}
+
+let idioma = "Alien";
+
+switch (idioma) {
+  case "español":
+    console.log("hola amigos");
+    break;
+  case "ingles":
+    console.log("hi friends");
+    break;
+  case "japones":
+    console.log("こんにちは、友だち！");
+    break;
+  case "Portugués":
+    console.log("Olá, amigos!");
+    break;
+  case "Francés":
+    console.log("Salut, les amis !");
+    break;
+  case "Alemán":
+    console.log("Hallo, Freunde!");
+    break;
+  case "Coreano":
+    console.log("안녕, 친구들!");
+    break;
+  case "Italiano":
+    console.log("Ciao, amici!");
+    break;
+  default:
+    console.log("idioma no registrado");
+}
+
+let nombreUsuario = prompt("ingresa tu nombre x favor :D");
+let edadUsuario = prompt("ingresa tu edad por favor");
+console.log(nombreUsuario);
+console.log(nombreUsuario.length);
+console.log(typeof edadUsuario);
+
+let edadDefinitiva = parseInt(edadUsuario);
+let edadDecimal = parseFloat(edadUsuario);
+console.log(typeof edadDefinitiva);
+console.log(edadDefinitiva);
+console.log(typeof edadDecimal);
+console.log(edadDecimal);
+
+/* GRUPO DE LOS NO HOMEROS */
+/* 
+pedimos al cliente q nos ingrese el nombre de usuario 
+si el nombre de usuario es Homero no se le debera permitir el ingreso al club 
+a menos que la cantidad de Homeros sea de 1 si es mayor o igual a 1 no se le permitira el ingreso al club
+*/

6dce567cc96bb5ff033756926b7ec765403212a9
kevindandrew <kevindandrew77@gmail.com>
2025-06-10 15:56:31 -0400

    eventos

---
+++ dia-17/main.js
@@ -0,0 +1,6 @@
+let btn = document.querySelector("#btn");
+let parrafo = document.querySelector("#parrafo");
+let contador = 0;
+btn.addEventListener("click", function (evento) {
+  console.log(evento);
+});

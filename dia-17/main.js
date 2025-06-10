cb0d11d33ab4a0d9c8ed427906b40d5116001fe1
kevindandrew <kevindandrew77@gmail.com>
2025-06-10 16:30:30 -0400

    ejercicio fiona

--- dia-17/main.js
+++ dia-17/main.js
@@ -1,6 +1,29 @@
-let btn = document.querySelector("#btn");
+/* let btn = document.querySelector("#btn");
 let parrafo = document.querySelector("#parrafo");
 let contador = 0;
+
 btn.addEventListener("click", function (evento) {
   console.log(evento);
+});
+ */
+
+let btn = document.querySelector("#btn");
+let img = document.querySelector("#imagen");
+let dia = true;
+
+btn.addEventListener("click", function () {
+  dia = !dia;
+  if (dia) {
+    btn.textContent = "Dia";
+    img.setAttribute(
+      "src",
+      "https://upload.wikimedia.org/wikipedia/en/d/d0/Fiona_standing_human.png"
+    );
+  } else {
+    btn.textContent = "noche";
+    img.setAttribute(
+      "src",
+      "https://upload.wikimedia.org/wikipedia/en/thumb/b/b9/Princess_Fiona.png/130px-Princess_Fiona.png"
+    );
+  }
 });

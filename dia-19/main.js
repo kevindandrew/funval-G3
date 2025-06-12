f19bd20ac4365b30bfa8264b853aaa7806302c53
kevindandrew <kevindandrew77@gmail.com>
2025-06-12 15:37:13 -0400

    ejercicio12

--- dia-19/main.js
+++ dia-19/main.js
@@ -47,7 +47,7 @@ console.log(capAraña);
 
 let a = [1, 2, 3];
 
-let b = [a, b, c];
+let b = ["a", "b", "c"];
 
 /* SALIDA ESPERADA:[1,a,2,b,3,c] */
 
@@ -59,3 +59,35 @@ let profesional = {
 /* agregar las llaves de turno y lista de pacientes q sera un array a ese objeto 
     convirtiendolo en un superProfesional :D
 */
+
+/* 
+Simular un sistema de gestión de usuarios que:
+
+Crea usuarios nuevos.
+
+Actualiza información de un usuario.
+
+Filtra usuarios por rol.
+
+Usa desestructuración y spread en todo momento.
+
+Tareas a resolver
+1. Mostrar todos los usuarios (usa desestructuración)
+Imprime en consola: "Usuario: Ana, Rol: admin" para cada usuario usando forEach y desestructuración.
+
+2. Agregar un nuevo usuario (usa spread)
+Crea una función agregarUsuario(lista, nuevo) que devuelva una nueva lista con el usuario nuevo agregado. No modifiques el array original.
+
+3. Actualizar un usuario por ID (usa spread)
+Crea una función actualizarUsuario(lista, id, nuevosDatos) que:
+
+Busque al usuario por id.
+
+Devuelva una nueva lista con los datos actualizados del usuario.
+
+Use spread para mantener los datos anteriores no modificados.*/
+const usuarios = [
+  { id: 1, nombre: "Ana", rol: "admin", edad: 28 },
+  { id: 2, nombre: "Luis", rol: "editor", edad: 34 },
+  { id: 3, nombre: "Carla", rol: "viewer", edad: 22 },
+];

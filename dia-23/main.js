ecb1b8bc7ca0306ea79ea36d05af0eac13868ba3
kevindandrew <kevindandrew77@gmail.com>
2025-06-18 15:36:54 -0400

    tienda perritos2

--- dia-23/main.js
+++ dia-23/main.js
@@ -29,8 +29,8 @@ for (let i = 1; i <= 10; i++) {
     .then((data) => {
       contenedor.innerHTML += `
         
-<a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow-sm md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-    <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="${data.message}" alt="">
+<a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow-sm md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 p-4">
+    <img class="object-cover w-full rounded-t-lg h-44  md:w-48 md:rounded-none md:rounded-s-lg" src="${data.message}" alt="">
     <div class="flex flex-col justify-between p-4 leading-normal">
         <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">perrito bonito ${i}</h5>
         <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">adopta un perrito hoy :D</p>
@@ -60,6 +60,25 @@ input.addEventListener("input", function () {
         div.className = "px-4 py-2 cursor-pointer hover:bg-blue-100";
         div.addEventListener("click", () => {
           input.value = item;
+
+          fetch(`https://dog.ceo/api/breed/${input.value}/images`)
+            .then((res) => res.json())
+            .then((data) => {
+              let arrayPerritos = data.message;
+              contenedor.innerHTML = "";
+              for (let j = 1; j <= 20; j++) {
+                contenedor.innerHTML += `
+                <a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow-sm md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 p-4">
+                    <img class="object-cover w-full rounded-t-lg h-44  md:w-48 md:rounded-none md:rounded-s-lg" src="${arrayPerritos[j]}" alt="">
+                    <div class="flex flex-col justify-between p-4 leading-normal">
+                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">perrito bonito ${j}</h5>
+                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">adopta un perrito hoy :D</p>
+                    </div>
+                </a>
+                `;
+              }
+            });
+
           dropdown.classList.add("hidden");
         });
         dropdown.appendChild(div);
